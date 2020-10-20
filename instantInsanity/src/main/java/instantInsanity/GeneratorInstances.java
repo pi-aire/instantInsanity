bc5b85a6de21174bab4e0a97e139b7fe64679694
pierre <vaslin.contact@gmail.com>
2020-10-20 14:37:20 +0200

    ajout json pom

--- instantInsanity/src/main/java/instantInsanity/GeneratorInstances.java
+++ instantInsanity/src/main/java/instantInsanity/GeneratorInstances.java
@@ -3,9 +3,11 @@ package instantInsanity;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.simple.JSONObject;
+
 public class GeneratorInstances {
     public static void main(String[] args) {
-
+        
         List<Instance> instances = new ArrayList<Instance>();
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 6; j++) {
@@ -16,6 +18,6 @@ public class GeneratorInstances {
             instances.add(new Instance(i));
         }
 
-        
+    
     }
 }
